64c14c162c5802631ab74f32addb9538316915e4
gowthamchi <130386327+gowthamchi@users.noreply.github.com>
2025-07-03 10:39:47 +0530

    Update create-pipeline.groovy

--- jenkins/create-pipeline.groovy
+++ jenkins/create-pipeline.groovy
@@ -18,12 +18,12 @@ if (Jenkins.instance.getItem(jobName) == null) {
     def job = Jenkins.instance.createProject(WorkflowJob, jobName)
 
     def scm = new GitSCM(
-        [new UserRemoteConfig("https://github.com/YOUR_USERNAME/nodejs-deployment-automation.git", null, null, null)],
+        [new UserRemoteConfig("https://github.com/gowthamchi/terraform-jenkins-ec2-setup.git", null, null, null)],
         [new BranchSpec("*/main")],
         false, [], null, null, []
     )
 
-    def flowDef = new CpsScmFlowDefinition(scm, "jenkins/Jenkinsfile")
+    def flowDef = new CpsScmFlowDefinition(scm, "jenkins/jenkinsfile")
     flowDef.setLightweight(true)
     job.setDefinition(flowDef)
 

819c1e71fc4c1b1350657c05a52c303ccdb53b35
gowthamchi <130386327+gowthamchi@users.noreply.github.com>
2025-07-03 10:36:12 +0530

    Update user_data.sh

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -3,8 +3,10 @@ apt update -y
 apt install -y docker.io git curl
 systemctl start docker
 systemctl enable docker
-mkdir -p /opt/jenkins_home/init.groovy.d
-curl -o /opt/jenkins_home/init.groovy.d/create-pipeline.groovy https://raw.githubusercontent.com/YOUR_USERNAME/nodejs-deployment-automation/main/jenkins/create-pipeline.groovy
+mkdir -p /opt/jenkins_home
+
+curl -o /opt/jenkins_home/create-pipeline.groovy https://raw.githubusercontent.com/gowthamchi/terraform-jenkins-ec2-setup/main/jenkins/create-pipeline.groovy
+
 docker run -d \
   --name jenkins \
   -p 8080:8080 \
